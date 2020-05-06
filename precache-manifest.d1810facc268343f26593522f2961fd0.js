268bc0e524db1d8dbc34b1c2a318b4be90832b50
butsarakham <butsarakham.k@gmail.com>
2020-05-07 00:39:50 +0700

    Updates

---
+++ precache-manifest.d1810facc268343f26593522f2961fd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2a69ed53f09169e6677a8ef8ead994",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "d110d417faf1fb7d0dd0",
+    "url": "/toDoList/static/css/main.44abebc7.chunk.css"
+  },
+  {
+    "revision": "26835184362d5f2fe97e",
+    "url": "/toDoList/static/js/2.e1549260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.e1549260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d110d417faf1fb7d0dd0",
+    "url": "/toDoList/static/js/main.44477170.chunk.js"
+  },
+  {
+    "revision": "fee74a0bf65ed2b7ce27",
+    "url": "/toDoList/static/js/runtime-main.186ffabc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
